50481e03a60c56bec8b98b523b4a897772c74f17
Matti <mjsillan@iki.fi>
2018-09-29 23:23:50 +0300

    Add frontend.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cors = require('cors')
 app.use(cors())
 app.use(bodyParser.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :json'))
+app.use(express.static('build'))
 
 morgan.token('json', function getJson (req) {
     return JSON.stringify(req.body)
