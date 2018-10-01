c689c34db7b0659ff9f6479208a8d25e8573d522
Matti <mjsillan@iki.fi>
2018-10-01 23:15:45 +0300

    Add Mongoose definitions for person.

---
+++ models/person.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+const url = process.env.MONGODB_URI
+
+mongoose.connect(url, { useNewUrlParser: true })
+
+const Person = mongoose.model('Person', {
+    name: String,
+    number: String,
+    id: Number
+})
+
+module.exports = Person
